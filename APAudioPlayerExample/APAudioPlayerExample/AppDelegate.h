66cde91530d070de88ef08f41be4aad2b8a86e93
Serg Krivoblotsky <krivoblotsky@me.com>
2015-05-14 14:33:51 +0300

    Example added, project updated

---
+++ APAudioPlayerExample/APAudioPlayerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APAudioPlayerExample
+//
+//  Created by Sergii Kryvoblotskyi on 5/14/15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
